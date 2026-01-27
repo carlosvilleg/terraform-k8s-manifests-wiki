bcb533e277fe8cd3c506c7b0b2fc3a525b7e065a
Carlos Villegas <carlos.villegas1@ibm.com>
2026-01-27 13:36:18 -0500

    read all resources from single manifest

--- base.tf
+++ base.tf
@@ -1,11 +1,12 @@
 
 resource "kubernetes_manifest" "base" {
-	manifest = provider::kubernetes::manifest_decode_multi(templatefile("${path.module}/base.yaml.tftpl", {
+  for_each = {for r in provider::kubernetes::manifest_decode_multi(templatefile("${path.module}/base.yaml.tftpl", {
     k8s_namespace = var.namespace,
     mysql_user = "",
     mysql_password = "",
     mysql_database = "",
     mysql_host = "",
-    }))
+    })): format("%s/%s/%s/%s", r.kind, r.apiVersion, r.metadata.namespace, r.metadata.name) => r}
+    manifest = each.value
 }
 
