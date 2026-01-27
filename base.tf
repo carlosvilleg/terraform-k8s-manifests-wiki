661548c7f49468aae0647925a7407d31d46916e3
Carlos Villegas <carlos.villegas1@ibm.com>
2026-01-27 17:26:05 -0500

    marked password as non sensitive

--- base.tf
+++ base.tf
@@ -3,7 +3,7 @@ resource "kubernetes_manifest" "base" {
   for_each = {for r in provider::kubernetes::manifest_decode_multi(templatefile("${path.module}/base.yaml.tftpl", {
     k8s_namespace = var.namespace,
     mysql_user = local.mysql_user,
-    mysql_password = random_password.password.result,
+    mysql_password = nonsensitive(random_password.password.result),
     mysql_database = aws_db_instance.wiki.db_name,
     mysql_host = aws_db_instance.wiki.endpoint,
     })): format("%s/%s/%s/%s", r.kind, r.apiVersion, try(r.metadata.namespace, "-"), r.metadata.name) => r}
