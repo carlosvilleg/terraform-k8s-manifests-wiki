4f0229fe23e4504087e065dc95fdc8d00822b900
Carlos Villegas <carlos.villegas1@ibm.com>
2026-01-27 17:21:46 -0500

    pass db values to secret template

--- base.tf
+++ base.tf
@@ -2,10 +2,10 @@
 resource "kubernetes_manifest" "base" {
   for_each = {for r in provider::kubernetes::manifest_decode_multi(templatefile("${path.module}/base.yaml.tftpl", {
     k8s_namespace = var.namespace,
-    mysql_user = "",
-    mysql_password = "",
-    mysql_database = "",
-    mysql_host = "",
+    mysql_user = local.mysql_user,
+    mysql_password = random_password.password.result,
+    mysql_database = aws_db_instance.wiki.db_name,
+    mysql_host = aws_db_instance.wiki.endpoint,
     })): format("%s/%s/%s/%s", r.kind, r.apiVersion, try(r.metadata.namespace, "-"), r.metadata.name) => r}
     manifest = each.value
 }
--- db.tf
+++ db.tf
@@ -1,4 +1,8 @@
 
+locals {
+  mysql_user = "wiki"
+}
+
 resource "random_password" "password" {
   length           = 16
   special          = false
@@ -9,7 +13,7 @@ resource "aws_db_instance" "wiki" {
   instance_class    = "db.t3.micro"
   allocated_storage = 10
   engine            = "mysql"
-  username          = "wiki"
+  username          = local.mysql_user
   password          = random_password.password.result
 }
 
