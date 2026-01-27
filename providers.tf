15943ed525aef18e153975129f4ac18ef55a60b1
Carlos Villegas <carlos.villegas1@ibm.com>
2026-01-27 14:35:37 -0500

    commented out kuberentes provider block

--- providers.tf
+++ providers.tf
@@ -7,11 +7,13 @@ data "aws_eks_cluster_auth" "cluster" {
   name = "example"
 }
 
+/*
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
+*/
 
 output "eks_host" {
   value = data.aws_eks_cluster.cluster.endpoint
