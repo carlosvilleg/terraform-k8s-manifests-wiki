4a427eb7612e938b5ea52574e6d2243c411730df
Carlos Villegas <136353783+carlosvilleg@users.noreply.github.com>
2026-02-03 18:13:09 -0500

    moved all db stuff to db.tf

--- db.tf
+++ db.tf
@@ -18,3 +18,21 @@ resource "aws_db_instance" "wiki" {
   password          = random_password.password.result
 }
 
+
+output "db_host" {
+	value = aws_db_instance.wiki.endpoint
+}
+
+output "db_database" {
+	value = aws_db_instance.wiki.db_name
+}
+
+output "db_user" {
+	value = local.mysql_user
+}
+
+output "db_password" {
+	value = random_password.password.result
+	sensitive = true
+}
+
