25ec8e24d441336c71d9e9cdbb4b63a3b224f0a2
Carlos Villegas <136353783+carlosvilleg@users.noreply.github.com>
2026-02-03 18:23:03 -0500

    added db stuff as variables

--- base.tf
+++ base.tf
@@ -4,10 +4,10 @@ resource "kubernetes_manifest" "base" {
     url = var.url,
     env = var.suffix,
     k8s_namespace = var.namespace,
-    mysql_user = local.mysql_user,
-    mysql_password = nonsensitive(random_password.password.result),
-    mysql_database = aws_db_instance.wiki.db_name,
-    mysql_host = aws_db_instance.wiki.endpoint,
+    mysql_user = var.mysql_user,
+    mysql_password = nonsensitive(var.mysql_password),
+    mysql_database = var.mysql_database,
+    mysql_host = var.mysql_host,
     })): format("%s/%s/%s/%s", r.kind, r.apiVersion, try(r.metadata.namespace, "-"), r.metadata.name) => r}
     manifest = each.value
 }
--- variables.tf
+++ variables.tf
@@ -5,15 +5,46 @@ variable "namespace" {
   description = "K8s namespace"
 }
 
+variable "create_namespace" {
+  type = bool
+  default = true
+  description = "Should the namespace be created?"
+}
+
 variable "suffix" {
 	type = string
 	default = "blue"
-  description = "Suffix for most resources, the db secret is shared"
+  description = "Suffix for most resources, the db secret is shared for all suffixes in the same namespace"
 }
 
 variable "url" {
 	type = string
 	default = "www.example.com"
   description = "URL for end users"
+}
+
+variable "mysql_user" {
+  type = string
+  default = "mysql"
+  description = "MySQL user to pass to the deployment in the environment"
+}
+
+variable "mysql_password" {
+  type = string
+  default = "mysql123"
+  description = "MySQL password to pass to the deployment in the environment"
+  sensitive = true
+}
+
+variable "mysql_host" {
+  type = string
+  default = "mysql"
+  description = "MySQL host to pass to the deployment in the environment (e.g. mydb.example.com)"
+}
+
+variable "mysql_database" {
+  type = string
+  default = "mysql"
+  description = "MySQL database name to pass to the deployment in the environment"
 }
 
