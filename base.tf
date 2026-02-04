7bdd40237ae8ac35d658cacf1f165ececcc0bc3a
Carlos Villegas <136353783+carlosvilleg@users.noreply.github.com>
2026-02-03 19:36:05 -0500

    fixed plan time issues for unknown db resources

--- base.tf
+++ base.tf
@@ -1,15 +1,43 @@
 
+data "kubernetes_namespace_v1" "ns" {
+  count = var.create_namespace ? 0 : 1
+  metadata {
+    name = var.namespace
+  }
+}
+
+resource "kubernetes_namespace_v1" "ns" {
+  count = var.create_namespace ? 1 : 0
+  metadata {
+    name = var.namespace
+  }
+}
+
+locals {
+  namespace = try(kubernetes_namespace_v1.ns[0].id, data.kubernetes_namespace_v1.ns[0].id)
+}
+
 resource "kubernetes_manifest" "base" {
   for_each = {for r in provider::kubernetes::manifest_decode_multi(templatefile("${path.module}/base.yaml.tftpl", {
     url = var.url,
     env = var.suffix,
     create_namespace = var.create_namespace,
     k8s_namespace = var.namespace,
+    })): format("%s/%s/%s/%s", r.kind, r.apiVersion, try(r.metadata.namespace, "-"), r.metadata.name) => r}
+    manifest = each.value
+
+    depends_on = [ local.namespace ]
+}
+
+resource "kubernetes_manifest" "mysql_secret" {
+	manifest = provider::kubernetes::manifest_decode(templatefile("${path.module}/secret.yaml.tftpl", {
+    k8s_namespace = var.namespace,
     mysql_user = var.mysql_user,
     mysql_password = nonsensitive(var.mysql_password),
     mysql_database = var.mysql_database,
     mysql_host = var.mysql_host,
-    })): format("%s/%s/%s/%s", r.kind, r.apiVersion, try(r.metadata.namespace, "-"), r.metadata.name) => r}
-    manifest = each.value
+    }))
+
+    depends_on = [ local.namespace ]
 }
 
