5d0b994139cfe3985bea7e140cdb909128de0ebf
Carlos Villegas <136353783+carlosvilleg@users.noreply.github.com>
2026-02-03 19:59:56 -0500

    never create the ns via manifest

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "kubernetes_manifest" "base" {
   for_each = {for r in provider::kubernetes::manifest_decode_multi(templatefile("${path.module}/base.yaml.tftpl", {
     url = var.url,
     env = var.suffix,
-    create_namespace = var.create_namespace,
+    create_namespace = false,
     k8s_namespace = var.namespace,
     })): format("%s/%s/%s/%s", r.kind, r.apiVersion, try(r.metadata.namespace, "-"), r.metadata.name) => r}
     manifest = each.value
